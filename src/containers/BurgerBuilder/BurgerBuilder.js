2f372b7348c8df4737e57a80dcc13c42a66d905a
Josh Newton <josh.newton@careerbuilder.com>
2018-01-19 23:19:29 -0500

    checking that no-ingredient message works

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,10 +7,10 @@ export default class BurgerBuilder extends Component {
 
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 3,
-      cheese: 1,
-      meat: 1
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
     }
   }
 
