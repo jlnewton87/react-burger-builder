f70f36a2674a3d016cb14700271de45793503f7c
Josh Newton <josh.newton@careerbuilder.com>
2018-01-20 10:21:50 -0500

    burger builder using controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import _ from 'lodash';
 
 import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
-export default class BurgerBuilder extends Component {
+const ingredientPrices = {
+  salad: .5,
+  cheese: .4,
+  meat: 1.3,
+  bacon: .7
+};
+
+class BurgerBuilder extends Component {
 
   state = {
     ingredients: {
@@ -11,15 +20,36 @@ export default class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0,
-    }
+    },
+    totalPrice: 4
+  };
+
+  addIngredient = (type) => {
+    let newIngredients = _.clone(this.state.ingredients);
+    const newCount = newIngredients[type] + 1;
+    newIngredients[type] = newCount;
+    const updatedPrice = this.state.totalPrice + ingredientPrices[type];
+    this.setState({ingredients: newIngredients, totalPrice: updatedPrice});
+  }
+
+  removeIngredient = (type) => {
+    let newIngredients = _.clone(this.state.ingredients);
+    const newCount = newIngredients[type] - 1;
+    newIngredients[type] = newCount;
+    const updatedPrice = this.state.totalPrice - ingredientPrices[type];
+    this.setState({ingredients: newIngredients, totalPrice: updatedPrice});
   }
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients}/>
-        <div>Builder Controls</div>
+        <BuildControls
+          addIngredientHandler={this.addIngredient}
+          removeIngredientHandler={this.removeIngredient} />
       </Aux>
     )
   }
 }
+
+export default BurgerBuilder;
