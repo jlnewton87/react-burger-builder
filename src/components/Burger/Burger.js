525192d5148b989653977cd0549e40d0eee37ed7
Josh Newton <josh.newton@careerbuilder.com>
2018-01-19 22:29:48 -0500

    using lodash instead of the Object and Array methods

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import _ from 'lodash';
 
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const ingredients = Object.keys(props.ingredients)
-    .map((igKey) => {
-      return [...Array(props.ingredients[igKey])].map((_, i) => {
-        return <BurgerIngredient key={igKey + i} type={igKey} />
-      });
-    });
+  const ingredients = _.reduce(props.ingredients, (output, v, k) => {
+    _.times(v, () => output.push(
+      <BurgerIngredient
+        key={ output.length + v + output.length }
+        type={k} />
+    ));
+    return output;
+  }, []);
   return (
     <div className={ classes.Burger }>
       <BurgerIngredient type='bread-top' />
