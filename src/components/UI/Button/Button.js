016346f5bdbce42584a2d0c00f5970beedfda1c6
Josh Newton <josh.newton@careerbuilder.com>
2018-02-05 14:12:01 -0500

    moving FA to the button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import FA from 'react-fontawesome';
 
 import classes from './Button.css';
 
+const getIcon = (className, iconType) => {
+  const iconName = [iconType, className].join('');
+  console.log(iconName)
+  const icons = {
+    sillysuccess: <FA name="smile-o" />,
+    sillydanger: <FA name="frown-o" />,
+    boringsuccess: <FA name="check-circle-o" />,
+    boringdanger: <FA name="times-circle-o" />
+  };
+
+  return icons[iconName];
+}
+
 const button = (props) => {
+  const icon = getIcon(props.type, props.iconType || 'boring')
   const classList = [ classes.Button ]
   classList.push(
     props.type === 'success' ?
@@ -13,7 +28,7 @@ const button = (props) => {
     <button
     onClick={props.clicked}
     className={classList.join(' ')}>
-      {props.children}
+      {icon} {props.children}
     </button>
   );
 };
