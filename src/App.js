b44719f8fb69d891fb599f554d530aba2ba79466
Josh Newton <josh.newton@careerbuilder.com>
2018-02-05 14:31:57 -0500

    add routing for checkout page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
+      <Router>
+        <div>
+          <Layout>
+              <Route path="/" component={BurgerBuilder} />
+              <Route path="/checkout" component={Checkout} />
+          </Layout>
+        </div>
+      </Router>
     );
   }
 }
