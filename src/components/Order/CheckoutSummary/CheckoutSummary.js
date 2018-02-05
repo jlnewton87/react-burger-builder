8c98b5ea15babbf58dbd7f0b300171ae0199e984
Josh Newton <josh.newton@careerbuilder.com>
2018-02-05 14:31:48 -0500

    getting checkout page started

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './CheckoutSummary.css';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
+const checkoutSummary = (props) => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>We hope it tastes great!</h1>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Burger ingredients={props.ingredients}/>
+      </div>
+      <Button
+        type="danger"
+        onClick >CANCEL</Button>
+      <Button
+        type="success"
+        onClick >CONTINUE</Button>
+    </div>
+  );
+};
+
+export default checkoutSummary;
